0a82af850fd3082edbbfe6495c0d47c37955a119
Xingkun Yin <xingkun.yin@ceridian.com>
2019-06-19 16:58:13 -0400

    Save the jsx which started today. WIll work on this at condo.

--- counter_app/src/components/counter.jsx
+++ counter_app/src/components/counter.jsx
@@ -3,7 +3,8 @@ import { SSL_OP_TLS_BLOCK_PADDING_BUG } from "constants";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
     // imageUrl: "https://picsum.photos/200"
   };
 
@@ -13,20 +14,24 @@ class Counter extends Component {
   };
 
   render() {
-    // let classes = "badge m-2";
-    // classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
-
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    let classes = this.getBadgeClasses();
 
     return (
       <div>
-        <span style={this.styles} className={classes}>
+        <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    // let classes = "badge m-2";
+    // classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
